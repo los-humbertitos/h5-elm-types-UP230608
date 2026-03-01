98b25772035ee4862757fee4efc5324899e805a5
Sebastian R <up230608@alumnos.upa.edu.mx>
2026-02-28 19:53:12 -0600

    Tarea 5 types

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,62 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+type AirplaneStatus
+    = Cancelled
+    | Delayed
+    | OnTime
+    | Boarding
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade list =
+    List.map
+        (\x ->
+            if x < 0 then
+                Pending
+
+            else if x < 7 then
+                Failed
+
+            else
+                Approved
+        )
+        list
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction vuelos =
+    List.map
+        (\vuelo ->
+            if vuelo == Cancelled then
+                "Pedir reembolso"
+
+            else if vuelo == Delayed || vuelo == OnTime then
+                "Esperar"
+
+            else
+                "Buscar boleto"
+        )
+        vuelos
